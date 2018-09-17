8dc2848658b16768174763563c6d8da539cecfb2
billedx <bill.sawarell@gmail.com>
2018-09-17 17:33:02 +0200

    addeed functions on new branch called addingone

--- code.js
+++ code.js
@@ -15,3 +15,26 @@ Person.prototype.sayHello = function()
 
 var bill = new Person('Bill');
 bill.sayHello();
+
+// adding stuff in a new branch called addingone
+// trying some math functions..
+
+
+console.log("tst..");
+
+function fifths(n)
+{
+    var x;
+    if (n > 0 && n <= 100)
+    {
+        x = Math.floor(n / 20);
+    }
+    else
+    {
+        x = "Incorrent value, input btw 0 and 100..";
+    }
+    return x;
+}
+
+var x = fifths(405);
+console.log(x);
